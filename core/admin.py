6273a45fd5c4ff3d4fe8ad53eb285502f1d78ed1
johnpauljpc <jpcwork081@gmail.com>
2023-02-05 10:17:56 +0100

    created core app

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Article, Author, articleSeries
+from .model2 import student
+
+
 
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(articleSeries)
+admin.site.register(Article)
+admin.site.register(student)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+class Author(models.Model):
+    pass
+
+class articleSeries(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=200)
+    slug = models.SlugField('Series slug', unique=True)
+    publised = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Series"
+        #ordering = ['-published']
+    
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=200, default="no subtittle", blank=True, null=True)
+    article_slug = models.SlugField('Article slug', unique=True)
+    content = models.TextField()
+    published = models.DateTimeField('publised date', auto_now_add=True)
+    modified = models.DateTimeField('modified date', auto_now=True)
+    series = models.ForeignKey(articleSeries, default="", on_delete=models.SET_DEFAULT)
+    # author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def slug(self):
+        return slug.article_slug
+
+    class Meta:
+        pass
+
+
+
+
